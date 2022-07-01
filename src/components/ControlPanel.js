bc26498c76d11f8c2386bf547f1089bd6ff120ae
T A Comer <tacomer@gmail.com>
2022-07-01 15:20:25 -0400

    Added weather API and local weather to control panel as a posssibility

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,10 +1,30 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GeocoderControl from './GeocoderControl';
 import TypeButton from './TypeButton';
+const WEATHER_API = process.env.REACT_APP_WEATHER_API
 
 const ControlPanel = React.forwardRef(({ MAPBOX_TOKEN, layerData, handleTrailLayers, trailLayers }, ref) => {
+
+  const [weather, setWeather] = useState({ description: '', temp: '' });
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=42&lon=-70&units=imperial&appid=${WEATHER_API}`).then(async (resp) => {
+          await resp.json().then((json) => {
+            console.log('json', { description: json.weather[0].main, temp: json.main.temp });
+            setWeather({ description: json.weather[0].main, temp: json.main.temp });
+            return json.weather[0].main;
+          });
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, []);
 
   const renderTypeButton = layerData.map((layer, index) => {
     return <TypeButton
@@ -24,6 +44,7 @@ const ControlPanel = React.forwardRef(({ MAPBOX_TOKEN, layerData, handleTrailLay
       <div>
         <h2>Find the trails that work for you!</h2>
         <p>Select from various trail types to find trails best suited to your needs.</p>
+        <p>Current Weather is: {weather.description} {weather.temp}</p>
       </div>
       <div>
         <h2>Existing:</h2>
