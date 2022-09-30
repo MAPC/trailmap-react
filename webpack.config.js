fd7db7a12452f506909f7b879918ef65518b4222
Tarin A. Comer <tcomer@mapc.org>
2022-09-30 14:11:02 -0400

    deployed to production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || 'development' || 'production',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
