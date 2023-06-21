e80426c0bfc1f0404dd1610463cbf3b32f3b70c8
NotLiu <aliu@lediur.com>
2023-06-21 12:23:58 -0400

    fix URL caps, re-style Direct Downloads section, add links to footer call for contribution

--- src/components/Modals/IntroModal.js
+++ src/components/Modals/IntroModal.js
@@ -6,22 +6,44 @@ import { LayerContext } from "../../App";
 
 const IntroModal = () => {
   const { showIntroModal, toggleIntroModal } = useContext(ModalContext);
+  const { showContributeModal, toggleContributeModal } = useContext(ModalContext);
   const { basemaps, setBaseLayer, setTrailLayers } = useContext(LayerContext);
-  const [assist, toggleAssist] = useState(false);
 
+  const [assist, toggleAssist] = useState(false);
 
   const handleCannedMap = (mapType) => {
     const setMap = (mapLayers) => {
-      setBaseLayer(basemaps.find(bm => bm.id == mapLayers.baseLayer));
+      setBaseLayer(basemaps.find((bm) => bm.id == mapLayers.baseLayer));
       setTrailLayers(mapLayers.trailLayers);
     };
 
     switch (mapType) {
-      case "hiking": setMap({ baseLayer: "terrain", trailLayers: ["unimprovedPaths", "naturalSurfaceFootway"] }); break;
-      case "biking": setMap({ baseLayer: "streets", trailLayers: ["bikeLane", "protectedBikeLane", "pavedPaths", "unimprovedPaths"] }); break;
-      case "walking": setMap({ baseLayer: "streets", trailLayers: ["pavedPaths", "pavedFootway", "unimprovedPaths"] }); break;
-      case "all": setMap({ baseLayer: "terrain", trailLayers: ['pavedPaths', 'bikeLane', 'pavedFootway', 'naturalSurfaceFootway', 'protectedBikeLane', 'unimprovedPaths'] });
-      default: break;
+      case "hiking":
+        setMap({ baseLayer: "terrain", trailLayers: ["unimprovedPaths", "naturalSurfaceFootway"] });
+        break;
+      case "biking":
+        setMap({
+          baseLayer: "streets",
+          trailLayers: ["bikeLane", "protectedBikeLane", "pavedPaths", "unimprovedPaths"],
+        });
+        break;
+      case "walking":
+        setMap({ baseLayer: "streets", trailLayers: ["pavedPaths", "pavedFootway", "unimprovedPaths"] });
+        break;
+      case "all":
+        setMap({
+          baseLayer: "terrain",
+          trailLayers: [
+            "pavedPaths",
+            "bikeLane",
+            "pavedFootway",
+            "naturalSurfaceFootway",
+            "protectedBikeLane",
+            "unimprovedPaths",
+          ],
+        });
+      default:
+        break;
     }
     toggleIntroModal(!showIntroModal);
   };
@@ -30,54 +52,96 @@ const IntroModal = () => {
     <Modal
       className="Modal"
       show={showIntroModal}
-      onHide={() => { toggleIntroModal(!showIntroModal); }}>
+      onHide={() => {
+        toggleIntroModal(!showIntroModal);
+      }}
+    >
       <Modal.Title>
         <span className="Modal__title text-center d-block mt-1 mb-1 ms-2 me-2 p-3 lh-lg">Welcome to Trailmap!</span>
-        <span className="Modal__subtitle text-center d-block mt-1 mb-1 ms-2 me-2 p-3">Metro Boston's Regional Walking and Cycling Map</span>
+        <span className="Modal__subtitle text-center d-block mt-1 mb-1 ms-2 me-2 p-3">
+          Metro Boston's Regional Walking and Cycling Map
+        </span>
       </Modal.Title>
       <Modal.Body className="Modal__body text-center">
-        {!assist &&
+        {!assist && (
           <>
             <Button
               className="intro-button mb-2"
               variant="primary"
-              onClick={() => { toggleAssist(!assist); }}>
+              onClick={() => {
+                toggleAssist(!assist);
+              }}
+            >
               Help Me Get Started
             </Button>
             <Button
               className="intro-button m-2"
               variant="primary"
-              onClick={() => { handleCannedMap("all"); }}>
+              onClick={() => {
+                handleCannedMap("all");
+              }}
+            >
               Show me Everything
             </Button>
-          </>}
-        {assist && <>
-          I'm interested in:
-          <div>
-            <Button
-              className="intro-button m-2"
-              variant="primary"
-              onClick={() => { handleCannedMap("biking"); }}>
-              Biking
-            </Button>
-            <Button
-              className="intro-button m-2"
-              variant="primary"
-              onClick={() => { handleCannedMap("hiking"); }}>
-              Hiking
-            </Button>
-            <Button
-              className="intro-button m-2"
-              variant="primary"
-              onClick={() => { handleCannedMap("walking"); }}>
-              Walking
-            </Button>
-          </div>
-        </>}
+          </>
+        )}
+        {assist && (
+          <>
+            I'm interested in:
+            <div>
+              <Button
+                className="intro-button m-2"
+                variant="primary"
+                onClick={() => {
+                  handleCannedMap("biking");
+                }}
+              >
+                Biking
+              </Button>
+              <Button
+                className="intro-button m-2"
+                variant="primary"
+                onClick={() => {
+                  handleCannedMap("hiking");
+                }}
+              >
+                Hiking
+              </Button>
+              <Button
+                className="intro-button m-2"
+                variant="primary"
+                onClick={() => {
+                  handleCannedMap("walking");
+                }}
+              >
+                Walking
+              </Button>
+            </div>
+          </>
+        )}
       </Modal.Body>
       <Modal.Footer>
-        <span className="Modal__footer text-center p-1">Trailmaps is always looking for new and improved data from the community. We encourage everyone to submit up-to-date infomration on individual trails so we can continue to improve this dataset. Learn more about trailmaps and continuing here.</span>
-        <span className="Modal__disclaimer p-1 fst-italic">Disclaimer: The data herein is provided for informational purposes only. MAPC makes no warranties, either expressed or implied, and assumes no responsibility for its completeness or accuracy. Users assume all responsibility and risk associated with use of the map and agree to indemnify and hold harmless MAPC with respect to any and all claims and demands that may arise resulting from use of this map.</span>
+        <span className="Modal__footer text-center p-1">
+          Trailmaps is always looking for new and improved data from the community. We encourage everyone to submit
+          up-to-date infomration on individual trails so we can continue to improve this dataset. Learn more about
+          trailmaps and continuing{" "}
+          <a
+            onClick={() => {
+              toggleIntroModal(false);
+              toggleContributeModal(true);
+            }}
+            className="modal-footer-link"
+          >
+            here
+          </a>
+          .
+        </span>
+        <span className="Modal__disclaimer p-1 fst-italic">
+          Disclaimer: The data herein is provided for informational purposes only. MAPC makes no warranties, either
+          expressed or implied, and assumes no responsibility for its completeness or accuracy. Users assume all
+          responsibility and risk associated with use of the map and agree to indemnify and hold harmless MAPC with
+          respect to any and all claims and demands that may arise resulting from use of this map.
+        </span>
       </Modal.Footer>
     </Modal>
   );
--- src/components/Modals/ShareModal.js
+++ src/components/Modals/ShareModal.js
@@ -9,6 +9,7 @@ import { ModalContext } from "../../App";
 
 const ShareModal = ({ url }) => {
   const { showShareModal, toggleShareModal } = useContext(ModalContext);
+  const { showContributeModal, toggleContributeModal } = useContext(ModalContext);
 
   const copyUrl = () => {
     navigator.clipboard.writeText(url);
@@ -24,19 +25,13 @@ const ShareModal = ({ url }) => {
       }}
     >
       <Modal.Title>
-        <span className="Modal__title text-center d-block mt-1 mb-1 ms-2 me-2 p-3 lh-lg">
-          Share the Map
-        </span>
+        <span className="Modal__title text-center d-block mt-1 mb-1 ms-2 me-2 p-3 lh-lg">Share the Map</span>
       </Modal.Title>
       <Modal.Body className="Modal__body text-center">
-        <span>Click below to copy the map url to share.</span>
+        <span>Click below to copy the map URL to share.</span>
         <InputGroup className="m-2">
           <Form.Control type="text" value={url} readOnly></Form.Control>
-          <Button
-            className="share-button p-0 d-flex align-items-center"
-            variant="outline-secondary"
-            onClick={copyUrl}
-          >
+          <Button className="share-button p-0 d-flex align-items-center" variant="outline-secondary" onClick={copyUrl}>
             <img src={CopyIcon} alt="Copy URL" />
           </Button>
         </InputGroup>
@@ -50,17 +45,12 @@ const ShareModal = ({ url }) => {
           </a>
         </span>
       </Modal.Body>
-      <span className="text-center">Direct Downloads</span>
+      <span className="text-center direct-download">Direct Downloads</span>
       <ul className="text-center direct-download-links">
         <li className="text-left direct-download-link">
-          <span className="direct-download-link-left">
-            Walking Trails (Lines)
-          </span>
+          <span className="direct-download-link-left">Walking Trails (Lines)</span>
           <div className="direct-download-link-right">
-            <a
-              href="https://datacommon.mapc.org/csv?table=mapc.trans_walking_trails&database=gisdata"
-              target="_blank"
-            >
+            <a href="https://datacommon.mapc.org/csv?table=mapc.trans_walking_trails&database=gisdata" target="_blank">
               <Button className="direct-download-link-btn">.csv</Button>
             </a>
             <a
@@ -72,14 +62,9 @@ const ShareModal = ({ url }) => {
           </div>
         </li>
         <li className="text-left direct-download-link">
-          <span className="direct-download-link-left">
-            Bicycle Facilities (Lines)
-          </span>
+          <span className="direct-download-link-left">Bicycle Facilities (Lines)</span>
           <div className="direct-download-link-right">
-            <a
-              href="https://datacommon.mapc.org/csv?table=mapc.trans_bike_facilities&database=gisdata"
-              target="_blank"
-            >
+            <a href="https://datacommon.mapc.org/csv?table=mapc.trans_bike_facilities&database=gisdata" target="_blank">
               <Button className="direct-download-link-btn">.csv</Button>
             </a>
             <a
@@ -91,9 +76,7 @@ const ShareModal = ({ url }) => {
           </div>
         </li>
         <li className="text-left direct-download-link">
-          <span className="direct-download-link-left">
-            Land Line Systems (Lines)
-          </span>
+          <span className="direct-download-link-left">Land Line Systems (Lines)</span>
           <div className="direct-download-link-right">
             <a
               href="https://datacommon.mapc.org/csv?table=mapc.trans_land_line_systems&database=gisdata"
@@ -110,9 +93,7 @@ const ShareModal = ({ url }) => {
           </div>
         </li>
         <li className="text-left direct-download-link">
-          <span className="direct-download-link-left">
-            Shared Use Trails (Lines)
-          </span>
+          <span className="direct-download-link-left">Shared Use Trails (Lines)</span>
           <div className="direct-download-link-right">
             <a
               href="https://datacommon.mapc.org/csv?table=mapc.trans_shared_use_paths&database=gisdata"
@@ -131,18 +112,25 @@ const ShareModal = ({ url }) => {
       </ul>
       <Modal.Footer>
         <span className="Modal__footer text-center">
-          Trailmaps is always looking for new and improved data from the
-          community. We encourage everyone to submit up-to-date infomration on
-          individual trails so we can continue to improve this dataset. Learn
-          more about trailmaps and continuing here.
+          Trailmaps is always looking for new and improved data from the community. We encourage everyone to submit
+          up-to-date infomration on individual trails so we can continue to improve this dataset. Learn more about
+          trailmaps and continuing{" "}
+          <a
+            onClick={() => {
+              toggleShareModal(false);
+              toggleContributeModal(true);
+            }}
+            className="modal-footer-link"
+          >
+            here
+          </a>
+          .
         </span>
         <span className="Modal__disclaimer fst-italic">
-          Disclaimer: The data herein is provided for informational purposes
-          only. MAPC makes no warranties, either expressed or implied, and
-          assumes no responsibility for its completeness or accuracy. Users
-          assume all responsibility and risk associated with use of the map and
-          agree to indemnify and hold harmless MAPC with respect to any and all
-          claims and demands that may arise resulting from use of this map.
+          Disclaimer: The data herein is provided for informational purposes only. MAPC makes no warranties, either
+          expressed or implied, and assumes no responsibility for its completeness or accuracy. Users assume all
+          responsibility and risk associated with use of the map and agree to indemnify and hold harmless MAPC with
+          respect to any and all claims and demands that may arise resulting from use of this map.
         </span>
       </Modal.Footer>
     </Modal>
