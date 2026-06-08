097c0c5de9f8fb855d01ab43a74b53a80cde3fdc
ztocode <miraclezag@gmail.com>
2026-06-08 18:32:33 -0400

    Refactor header navigation and dashboard route
    Add primary nav, dashboard placeholder, header modal toggling, and move Header inside LayerContext for map layer access.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,68 +1,70 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ModalContext } from "../../App";
 import Control from "../Map/Control";
+import HeaderNav from "./HeaderNav";
 import AboutIcon from "../../assets/icons/about-icon.svg";
 import GlossaryIcon from "../../assets/icons/glossary-icon.svg";
-import ContributeIcon from "../../assets/icons/contribute-icon.svg";
 import HelpIcon from "../../assets/icons/help-icon.svg";
 import TrailmapLogo from "../../assets/MAPC_logo.svg";
 
 const Header = () => {
-  const {
-    showAboutModal,
-    toggleAboutModal,
-    showContributeModal,
-    toggleContributeModal,
-    showIntroModal,
-    toggleIntroModal,
-    showGlossaryModal,
-    toggleGlossaryModal,
-  } = useContext(ModalContext);
+  const { toggleHeaderModal } = useContext(ModalContext);
 
   return (
     <header className="Header d-flex flex-row align-items-center position-absolute">
-      <a href="https://www.mapc.org/resource-library/landline-vision-plan/#google_translate_element" target="_blank">
+      <Link className="Header__brand" to="/" aria-label="Trailmap home">
         <img src={TrailmapLogo} className="Header__image" alt="Metropolitan Area Planning Council" />
-      </a>
+      </Link>
+
       <div className="Header__title">
         <span className="Header__title--main Header__title--small-mobile-remove">
           Trailmap
           <span className="Header__title--mobile-remove">:</span>
         </span>
         <span className="Header__title--subtitle Header__title--mobile-remove ps-2">
-          Metro Boston's Regional Walking and Cycling Map
+          Metro Boston&apos;s Regional Walking and Cycling Map
         </span>
+      </div>
+
+      <HeaderNav />
+
+      <div className="Header__actions">
         <Control
-          style={"Header__contribute d-flex justify-content-evenly align-items-center m-0 p-0 position-absolute"}
-          icon={ContributeIcon}
-          alt={"Contribute Map"}
-          clickHandler={() => {
-            toggleContributeModal(!showContributeModal);
-          }}
+          style="Header__action Header__contribute"
+          iconClass="bi bi-pencil-square"
+          alt="Contribute trail info"
+          tooltip="Contribute trail info"
+          tooltipId="header-contribute-tooltip"
+          tooltipPlacement="left"
+          clickHandler={() => toggleHeaderModal("contribute")}
         />
         <Control
-          style={"Header__about d-flex justify-content-evenly align-items-center m-0 p-0 position-absolute"}
+          style="Header__action Header__about"
           icon={AboutIcon}
-          alt={"About Map"}
-          clickHandler={() => {
-            toggleAboutModal(!showAboutModal);
-          }}
+          alt="About Trailmap"
+          tooltip="About Trailmap"
+          tooltipId="header-about-tooltip"
+          tooltipPlacement="left"
+          clickHandler={() => toggleHeaderModal("about")}
         />
         <Control
-          style={"Header__glossary d-flex justify-content-evenly align-items-center m-0 p-0 position-absolute"}
+          style="Header__action Header__glossary"
           icon={GlossaryIcon}
-          alt={"About Trails"}
-          clickHandler={() => {
-            toggleGlossaryModal(!showGlossaryModal);
-          }}
+          alt="Glossary of trail types"
+          tooltip="Glossary of trail types"
+          tooltipId="header-glossary-tooltip"
+          tooltipPlacement="left"
+          clickHandler={() => toggleHeaderModal("glossary")}
         />
         <Control
-          style={"Header__assist d-flex justify-content-evenly align-items-center m-0 p-0 position-absolute"}
+          style="Header__action Header__assist"
           icon={HelpIcon}
-          alt={"About Map"}
-          clickHandler={() => {
-            toggleIntroModal(!showIntroModal);
-          }}
+          alt="Help and getting started"
+          tooltip="Help & getting started"
+          tooltipId="header-help-tooltip"
+          tooltipPlacement="left"
+          clickHandler={() => toggleHeaderModal("intro")}
         />
       </div>
     </header>
